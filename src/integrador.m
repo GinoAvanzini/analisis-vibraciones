c047985af450094f0c995833450fc9216e3e7f73
Gino Avanzini <gino.avanzini@hotmail.com>
2019-06-05 21:28:55 -0300

    Update .gitignore

--- src/integrador.m
+++ src/integrador.m
@@ -1,7 +1,7 @@
 
 % 
-% Proyecto Integrador de Mecánica Vibratoria
-% Análisis de Vibraciones en vehículo con simetría axial de 4 GDL
+%% Proyecto Integrador de Mecánica Vibratoria
+%% Análisis de Vibraciones en vehículo con simetría axial de 4 GDL
 %
 
 
@@ -9,8 +9,8 @@ clc
 clear all
 close all
 
-%%
-% Datos del sistema
+%% Datos del sistema
+% 
 
 m1 = 25;    % Kg
 m2 = m1;    % Kg
@@ -30,8 +30,8 @@ l2 = 1;     % m
 
 gdl = 4;
 
-dt = 0.0025;
-t = 0:dt:50;
+dt = 0.0001;    % f = 10kHz
+t = 0:dt:5;
 
 len_t = length(t);
 
@@ -46,8 +46,8 @@ xd0 = [0;
     0;
     0];
 
-%%
-% Matrices
+%% Matrices
+% 
 
 M = [m1, 0, 0, 0;
     0, m2, 0, 0;
@@ -69,10 +69,10 @@ L = [k2, 0, 0, 0;    % Matriz de participación de carga. Pn = L*P(t)
     0, 0, 0, 0; 
     0, 0, 0, 0];
 
-zeta_n = [0.0005;
-    0.001;
-    0.0275;
-    0.028];
+% zeta_n = [0.0005;
+%     0.001;
+%     0.0275;
+%     0.028];
 
 [FI, Wn] = eig(K, M);
 [X, wx] = eig(M\K);
@@ -82,199 +82,75 @@ Mn = diag(X'*M*X);
 Kn = diag(X'*K*X);
 
 Wn_vec = diag(Wn); % Los wn son en realidad w^2
-Wnd = Wn_vec.^0.5 .*(1-zeta_n.^2).^0.5;
+% Wnd = Wn_vec.^0.5 .*(1-zeta_n.^2).^0.5;
 
 
 % Descripción de la carga
-A = 0.04;  
+A = 0.04;
 lambda = 0.35;
 
-v = 10*1000/3600;       % 40 km/h en m/s
+v = 100*1000/3600;       % 40 km/h en m/s
 wl = 2*pi*v/lambda;     % frec de la carga, función de la vel del auto
 
 
-%%
-% Respuesta en vibraciones libres analíticamente
-
-y = zeros(4, len_t);
-
-% Condiciones iniciales en coordenadas modales
-y0 = FI'*M*x0;
-yd0 = FI'*M*xd0;
-
-for i = 1:gdl
-    
-   y(i, :) = (y0(i)*cos(Wnd(i).*t) ...
-   + ((yd0(i) + Wn_vec(i)^0.5*zeta_n(i)*y0(i))./Wnd(i)) ...
-   .*sin(Wnd(i).*t)).*exp(-zeta_n(i).*Wn_vec(i)^0.5.*t);
-    
-end
-
-% Transformación a coordenadas geométricas
-x = FI*y;
-
-for i = 1:gdl
-    
-    figure(i)
-    plot(t, x(i, :))
-    
-end
-
-%% 
-% Respuesta a carga periódica armónica de forma analítica
-
-
-beta_n = wl./(Wn_vec.^0.5);
-yf = zeros(4, len_t);
-
-
-P0f = FI'*(A*diag(L));           % Amplitud de carga generalizada
-
-D = ((1-beta_n.^2).^2 + (2.*zeta_n.*beta_n).^2).^-0.5;
-theta = atan(2.*zeta_n.*beta_n./(1-beta_n.^2));
-
-
-for i=1:gdl
-    
-    yf(i, :) = (P0f(i)*D(i)/Kn(i)*sin(wl*t-theta(i)));
-    
-end
-
-xf = FI*yf;
-for i = 1:gdl
-    
-    figure(i)
-    plot(t, xf(i, :))
-    
-end
-
-
-%%
-% Respuesta a carga armónica con ode45
-
-
-P0_m = P0f./Mn;
-y_ode_total = zeros(gdl, len_t);
-
-for i = 1:gdl
-    
-    [t, y_ode] = ode45(@(t, y_ode) vibforz(t, y_ode, zeta_n(i), Wn_vec(i)^0.5, P0_m(i), wl), t, [y0(i); yd0(i)]);
-    
-    y_ode_total(i, :) = y_ode(:, 1);
-
-    figure(2)
-    plot(t, y_ode_total(i, :)) % y(:, 2) es la derivada de y(:, 1)
-    hold on
-    
-    
-%     figure(3)
-%     error__ = x(i, :) - y(:, 1)';
-%     plot(t, error__)
-%     hold on
-
-end
-
-x_ode = FI*y_ode_total;
-figure(5)
-plot(t, x_ode(1, :)) % y(:, 2) es la derivada de y(:, 1)
-hold on
-
-
-%%
-% Respuesta a carga impulsiva mediante la Integral de Duhamel
-
-% a = 0.05;
-% b = 0.04;
+%% Respuesta con el ode45 en coordenadas geométricas
 % 
-% v = 20*1000/3600;
-% 
-% % pico = ((a/2)/v)
-% 
-% % tri = triangularPulse(0, (a/2)/v, 0.8, t);
-% 
-% % figure(8)
-% % plot(t, tri)
-% 
-% P = k2*A.*sin(wl*t);
-% 
-% 
-% y_duha = zeros(4, len_t);
-% 
-% for i=1:gdl
-%     
-%     yc = P.*cos(Wn_vec(i)^0.5*t);
-%     ys = P.*sin(Wn_vec(i)^0.5*t);
-% 
-%     
-%     A_d = zeros(1, len_t);
-%     B_d = zeros(1, len_t);
-% 
-%     decr = exp(-zeta_n(i)*Wn_vec(i)^0.5*dt);
-%     
-%     for j = 2:len_t
-%         
-%         A_d(j) = A_d(j-1)*decr + ...
-%             (dt/(2*Mn(i)*Wn_vec(i)^0.5))*(yc(j-1)*decr + yc(j));
-%         B_d(j) = B_d(j-1)*decr + ...
-%             (dt/(2*Mn(i)*Wn_vec(i)^0.5))*(ys(j-1)*decr + ys(j));
-%         
-%     end
-%     
-%     aa = A_d.*sin(Wnd(i)*t);
-%     bb = B_d.*cos(Wnd(i)*t);
-%     y_duha(i, :) = aa - bb;
-% 
-% end
-% 
-% for i=1:gdl
-%     figure(i)
-%     plot(t, y_duha(i, :))
-% end
 
-
-%% 
-% Respuesta con el ode45 en coordenadas geométricas
-
+clc
 
 phase = (((l1 + l2)/lambda) - floor((l1 + l2)/lambda))*2*pi;
 
-A = 0.04;  
-lambda = 0.35;
-
-v = 200*1000/3600;       % 40 km/h en m/s
-wl = 2*pi*v/lambda;     % frec de la carga, función de la vel del auto
-
-xg = [A*sin(wl*(t'));
-    A*sin(wl*(t') - phase);
+xg = [A*sin(wl*(t));
+    A*sin(wl*(t) - phase);
     zeros(1, len_t);
     zeros(1, len_t)];
 
+% for i=1:gdl
+%     figure(i)
+%     plot(t, xg(i, :))
+% 
+% end
+
+x0 = [0.010; 0.010; 0.01; 0.1];
+
 P0 = L*xg;
-% figure(1)
-% plot(t, P0(1, :))
-% figure(2)
-% plot(t, P0(2, :))
 
 ZERO = zeros(gdl, gdl);
 I = eye(gdl, gdl);
-[t, x_ode] = ode45(@(t, x_ode) ...
-    vibforz_geo(t, x_ode, M, C, K, ZERO, I, L, wl, phase, A), t, [x0; xd0]);
-
-% x0 = [0.01; 0; -0.2; 0];
+% 
 % [t, x_ode] = ode45(@(t, x_ode) ...
-%     vibforz_geo(t, x_ode, M, C, K, ZERO, I, L, wl, 0, 0), t, [x0; xd0]);
+%     vibforz_geo(t, x_ode, M, C, K, ZERO, I, L, wl, phase, A), t, [x0; xd0]);
 
-x_ode = x_ode';
+[t, x2_ode] = ode45(@(t, x2_ode) ...
+    vibforz_geo(t, x2_ode, M, C, K, ZERO, I, L, wl, phase, A), t, [x0; xd0]);
+
+x2_ode = x2_ode';
 
 for i=1:gdl
     figure(i)
-    plot(t, x_ode(i, :))
+    plot(t, x2_ode(i, :))
 
 end
 
+% setGlobalcounter(1);
+% [t, x_ode] = ode45(@(t, x_ode) ...
+%     vibforz_geom(t, x_ode, M, C, K, ZERO, I, L, xg), t, [x0; xd0]);
 
-%% 
-% Función para el ode45 en coordenadas modales
+% x_ode = x_ode';
+% 
+% for i=1:gdl
+%     figure(i)
+%     plot(t, x_ode(i, :))
+% 
+% end
+
+%% Método de diferencia central para respuesta ante carga general
+
+
+
+
+%% Función para el ode45 en coordenadas modales
+% 
 
 function yd = vibforz(t, y, z, w, P0m, wL)
 
@@ -286,8 +162,10 @@ function yd = vibforz(t, y, z, w, P0m, wL)
 
 end
 
-%%
-% Función para el ode45 en coordenadas geométricas
+
+%% Función para el ode45 en coordenadas geométricas
+%
+
 function x_forz = vibforz_geo(t, x, M, C, K, ZERO, I, L, wl, phase, amp)
 
     A = [ZERO, I;
@@ -302,3 +180,35 @@ function x_forz = vibforz_geo(t, x, M, C, K, ZERO, I, L, wl, phase, amp)
     x_forz = A*x + B;
 
 end
+
+% function x_forz = vibforz_geom(t, x, M, C, K, ZERO, I, L, xg)
+%     
+%     if (getGlobalcounter() > 150001)
+% %         x_forz = zeros(8, 1);
+%         setGlobalcounter(getGlobalcounter()-1);
+%         disp("hola")
+%     end
+%     
+%     a = [ZERO, I;
+%         -inv(M)*K, -inv(M)*C];
+%     
+%     
+%     B=vertcat(zeros(4, 1), -inv(M)*(L*xg(:, getGlobalcounter())));
+%     
+%     x_forz = a*x + B;
+% 
+%     setGlobalcounter(getGlobalcounter()+1);
+%         
+% end
+% 
+% function setGlobalcounter(val)
+% global counter
+% counter = val;
+% 
+% end
+% 
+% function r = getGlobalcounter()
+% global counter
+% r = counter;
+% end
+
